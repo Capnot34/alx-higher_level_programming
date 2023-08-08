6b131054aab3d56813dd2dcf289f8d310da88e0d
Capnot34 <jenniferamachree2@gmail.com>
2023-08-08 15:28:29 +0000

    doen

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(90):
-    if i % 10 != i // 10:  # exclude numbers with similar digits
-        if i < 99:
-            print("{:02d}".format(i), end=" ,")
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{:02d}".format(i))
+            print("{:d}{:d}".format(i, j), end=", ")
