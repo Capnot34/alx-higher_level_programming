09582b1bc0eadb333b444ebf4282e22eb807eae6
Capnot34 <jenniferamachree2@gmail.com>
2023-08-08 16:28:00 +0000

    done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{:c}".format(ord('a')+i if i%2 == 0 else ord('A')+i), end='')
+for i in range(122, 96, -1):
+    char = i
+    if i % 2 != 0:
+        char = char - 32
+    print("{:c}".format(char), end="")
