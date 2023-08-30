ba74506d86dabc7882ec9a0aaf0fc1b159be48f1
Capnot34 <jenniferamachree2@gmail.com>
2023-08-30 18:51:26 +0000

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+	"""A class that defines a square with a private attribute size
+	"""
+
+	def __init__(self, size):
+		"""Initialize the square with a size"""
+		self.__size = size
+
