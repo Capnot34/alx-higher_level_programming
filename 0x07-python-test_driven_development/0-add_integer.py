ffe6cf284a7a7c403ac802894b9fcf54ee5fb747
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-09-06 09:19:27 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -37,6 +37,12 @@ def add_integer(a, b=98):
         ...
     TypeError: a must be an integer
     """
+    # Check if a or b is NaN
+    if isinstance(a, float) and a != a:
+        raise TypeError("a must be an integer")
+    if isinstance(b, float) and b != b:
+        raise TypeError("b must be an integer")
+
     # Check if a is neither an integer nor a float
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
