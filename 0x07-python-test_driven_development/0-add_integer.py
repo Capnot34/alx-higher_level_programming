2582eb6ca4265ffd7e70ef948a95da94b2e25472
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-09-06 10:53:36 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""
-This module contains a function to add two numbers: `add_integer`.
+"""Module for add_integer method"""
 
-The function can add two integers or floats. If the numbers are floats, they
-are cast to integers before the addition. If either of the numbers isn't an
-integer or a float, the function raises a TypeError.
-
-You can use this module as a script to run doctests or import the `add_integer`
-function in other modules.
-"""
 
 def add_integer(a, b=98):
-    """
-    Add two numbers (integers/floats). If they are floats, they are casted to
-    integers before addition.
+    """Adds two integers.
 
     Args:
-    a : int or float
-        The first number.
-    b : int or float, optional
-        The second number (default is 98).
-
-    Returns:
-    int
-        The sum of a and b.
+        a: first int.
+        b: second int, default value is 98.
 
     Raises:
-    TypeError
-        If a or b are neither int nor float.
+        TypeError: if a, b are neither int nor float.
+
+    Returns:
+        sum of a and b.
     """
 
     if type(a) not in (int, float):
@@ -37,6 +23,6 @@ def add_integer(a, b=98):
     else:
         return int(a) + int(b)
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/0-add_integer.txt")
