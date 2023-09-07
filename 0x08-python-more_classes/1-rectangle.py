476a4f2e868331b72efeaa71211537db1be43b65
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-09-07 19:19:52 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,21 +4,19 @@ Module 1-rectangle
 Defines a Rectangle.
 """
 
+
 class Rectangle:
     """Represents a Rectangle."""
 
-    
     def __init__(self, width=0, height=0):
         """Initializes the rectangle."""
         self.height = height
         self.width = width
-
     
     @property
     def width(self):
         """Getter for width."""
         return self.__width
-
     
     @width.setter
     def width(self, value):
@@ -28,14 +26,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
     
     @property
     def height(self):
         """Getter for height."""
         return self.__height
 
-    
     @height.setter
     def height(self, value):
         """Setter for height."""
