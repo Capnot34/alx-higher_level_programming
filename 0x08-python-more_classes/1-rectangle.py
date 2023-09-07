28576fe959a73dd973db276606daf5348f804d83
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-09-07 19:24:32 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
     def width(self):
         """Getter for width."""
         return self.__width
-    
+        
     @width.setter
     def width(self, value):
         """Setter for width."""
@@ -26,7 +26,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+        
     @property
     def height(self):
         """Getter for height."""
