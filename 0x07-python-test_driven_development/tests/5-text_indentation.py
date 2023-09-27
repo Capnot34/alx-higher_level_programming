d7be1dcac9afe97c07fec63f0d83bb2b77dcefe5
Capnot34 <jenniferamachree2@github.com>
2023-09-27 01:20:51 -0700

    done

---
+++ 0x07-python-test_driven_development/tests/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+    - text: The input text.
+
+    Returns:
+    None. The function will print the indented text.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delimiter in ['.', '?', ':']:
+        text = text.replace(delimiter, delimiter + '\n\n')
+
+    for line in text.split('\n'):
+        print(line.strip())
+
+if __name__ == "__main__":
+    text_indentation("Hello, World! How are you? I hope all is well.")
