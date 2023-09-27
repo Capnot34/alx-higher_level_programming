e798925061e5f7c41381d28feb7713821afcb5ab
Capnot34 <jenniferamachree2@github.com>
2023-09-27 01:59:07 -0700

    done

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,6 +2,7 @@
 """
 This module provides a function to indent a given text after certain punctuation marks.
 """
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :
@@ -18,8 +19,8 @@ def text_indentation(text):
     for delimiter in ['.', '?', ':']:
         text = text.replace(delimiter, delimiter + '\n\n')
 
-    for line in text.split('\n'):
-        print(line.strip())
+    text = '\n'.join([line.strip() for line in text.split('\n')]):
+        print(text)
 
 if __name__ == "__main__":
     text_indentation("Hello, World! How are you? I hope all is well.")
