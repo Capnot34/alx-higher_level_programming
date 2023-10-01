476a049ea529ae8f13fa1b171d01b9c0d571fc42
Capnot34 <jenniferamachree2@gmail.com>
2023-10-01 18:13:29 +0000

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
 	"""
-	This function reads the content of the given text file and prints it to stdout.
+	Read the content of a file and print it to stdout.
 
-	>>> read_file("tests/sample_test.txt")
-    Hello, this is a test file!
+	Args:
+		filename (str): The path to the file to be read. Defaults to an empty string.
+	Returns:
+		None: This function doesn't return anything; it prints the file's content.
+	Raises:
+		FileNotFoundError: If the provided filename does not exist.
+	
+	Example:
+		>>> read_file("tests/sample_test.txt")
+		Hello, this is a test file!
 
 	"""
 	with open(filename, 'r', encoding='utf-8') as file:
 		content = file.read()
-		print(content)
-
+		print(content, end='')
