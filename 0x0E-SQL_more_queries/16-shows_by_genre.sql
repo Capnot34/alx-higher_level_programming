90ad27e9ceccf2a58e139b9eb4a8f5d5a3e3c16d
Capnot34 <jenniferamachree2@github.com>
2023-10-20 04:59:27 -0700

    done

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,13 +1,13 @@
 -- Lists all shows and their genres (if any)
 SELECT 
-    tv_shows.title AS "tv_shows.title", 
-    COALESCE(tv_genres.name, 'NULL') AS "tv_genres.name"
+    tv_genres.name
 FROM 
-    tv_shows
-LEFT JOIN 
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN 
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    tv_genres
+JOIN 
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN 
+    tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE 
+    tv_shows.title = 'Dexter'
 ORDER BY 
-    tv_shows.title ASC, 
-    "tv_genres.name" ASC;
+    tv_genres.name ASC;
