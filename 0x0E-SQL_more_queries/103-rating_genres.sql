8d3006def4e56c7a6291a0f0bac1b4c7efba9d93
Capnot34 <jenniferamachree2@github.com>
2023-10-20 04:23:11 -0700

    done

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,14 +1,7 @@
 -- Lists all genres sorted by their rating in descending order
-SELECT 
-    tg.name AS genre_name,
-    COALESCE(SUM(tr.rating_value), 0) AS rating_sum
-FROM 
-    tv_genres tg
-LEFT JOIN 
-    tv_show_genres tsg ON tg.id = tsg.genre_id
-LEFT JOIN 
-    tv_show_ratings tr ON tsg.show_id = tr.show_id
-GROUP BY 
-    tg.name
-ORDER BY 
-    rating_sum DESC;
+SELECT tv_genres.name AS name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
