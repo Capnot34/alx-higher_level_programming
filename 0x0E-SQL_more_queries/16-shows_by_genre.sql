f965edc649fece4278e45f9a5b8b807d39acba10
Capnot34 <jenniferamachree2@github.com>
2023-10-20 04:53:26 -0700

    done

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,13 +1,13 @@
 -- Lists all shows and their genres (if any)
-SELECT
-    tv_shows.title,
-    IFNULL(tv_genres.name, 'NULL') AS genre
-FROM
+SELECT 
+    tv_shows.title AS "tv_shows.title", 
+    COALESCE(tv_genres.name, 'NULL') AS "tv_genres.name"
+FROM 
     tv_shows
-LEFT JOIN
+LEFT JOIN 
     tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN
+LEFT JOIN 
     tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY
-    tv_shows.title ASC,
-    genre ASC;
+ORDER BY 
+    tv_shows.title ASC, 
+    "tv_genres.name" ASC;
