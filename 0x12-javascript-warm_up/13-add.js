9efdcc97f2c948b8961923fe6c1c23218d2be396
Capnot34 <jenniferamachree2@github.com>
2023-11-14 14:39:37 -0800

    done

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-#!/usr/bin/node
 
 // Function to add two integers
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
 // Export the add function
 module.exports = {
-  add: add
+  add
 };
