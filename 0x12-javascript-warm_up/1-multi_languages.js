9629ebdd6abfa3e2b7526221777343c30e732959
Capnot34 <jenniferamachree2@gmail.com>
2023-11-14 15:47:31 +0000

    done

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-
-const myVars = { i: "C is fun", b: "Python is cool", c: "JavaScript is amazing" };
+const myVars = {
+    i: "C is fun",
+    b: "Python is cool",
+    c: "JavaScript is amazing"
+};
 
 for (const items in myVars) {
     console.log(myVars[items]);
