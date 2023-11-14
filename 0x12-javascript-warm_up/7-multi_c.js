49efc3f03cc54557c239334a924abbe43d9bebb2
Capnot34 <jenniferamachree2@github.com>
2023-11-14 13:22:49 -0800

    done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-function multiply (str, num) {
-  if (typeof num !== 'number') {
-    console.log('Missing number of occurrences');
-  } else {
-    let result = '';
-    for (let i = 0; i < num; i++) {
-      result += str;
-    }
-    return result;
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
