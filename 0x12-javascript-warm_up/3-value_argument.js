7a1db03275dc552b1096c09fb09618b1ab7283d3
Capnot34 <jenniferamachree2@github.com>
2023-11-25 10:05:13 -0800

    task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Check if an argument is passed
+if (process.argv[2]) {
+  // If argument is passed, print it
+  console.log(process.argv[2]);
+} else {
+  // If no argument is passed, print "No argument"
+  console.log("No argument");
+}
