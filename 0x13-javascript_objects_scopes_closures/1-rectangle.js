cff8b92a3942827a66784e2e1b7bbcbfd0ceefaa
Capnot34 <jenniferamachree2@github.com>
2023-12-03 06:32:05 -0800

    task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,18 +1,7 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
+module.exports = class Rectangle {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
-}
-
-module.exports = Rectangle;
-
-// Example usage
-const Rectangle = require('./1-rectangle');
-
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+};
