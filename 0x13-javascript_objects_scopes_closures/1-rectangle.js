c27f565b9aa5331c104ba17a232e1ceb682a7dff
Capnot34 <jenniferamachree2@github.com>
2023-12-03 06:29:02 -0800

    task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    // Check if both width and height are greater than 0
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 
-// Export the Rectangle class
 module.exports = Rectangle;
+
+// Example usage
+const Rectangle = require('./1-rectangle');
+
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
