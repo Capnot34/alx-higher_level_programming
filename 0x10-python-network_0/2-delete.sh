f08c685871ec3747829e95c62fcd636ac09f97ff
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-12-05 20:57:17 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response for a 200 status code
-
 curl -s "$1" -X GET -i | awk '/HTTP\/1.0 200 OK/{flag=1; next} flag; /./{flag=0}'
