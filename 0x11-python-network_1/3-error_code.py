cf7d4d9402814fbd6aecf13c497f931d2cf78167
Gift Amachree <128798186+Capnot34@users.noreply.github.com>
2023-12-05 21:47:40 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """
-Takes in a URL, sends a request to the URL, and displays the body of the response (decoded in utf-8).
-Handles urllib.error.HTTPError exceptions and prints the HTTP status code.
+takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
 """
-
 import urllib.request
-import urllib.error
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
+    """
+    takes in a URL and an email, sends a POST request to the passed
+    URL with the email as a parameter, and displays the body of the
+    response (decoded in utf-8)
+    """
+    url = argv[1]
+    req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read().decode('utf-8')
-            print(content)
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            html_str = html.decode('utf-8')
+            print(html_str)
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
