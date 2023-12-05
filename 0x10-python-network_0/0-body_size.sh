bcaf7b29d8f5faa0a9aa46a7df9c57585ca8962b
Capnot34 <jenniferamachree2@github.com>
2023-12-05 11:49:26 -0800

    some tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-# Check if the number of arguments is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Use curl to send a request and display the size of the body in bytes
-curl -sI "$url" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response for a 200 status code
 
-# Sends a DELETE request to the URL and displays the body of the response
-curl -sX DELETE "$1"
+curl -s "$1" -X GET -i | awk '/HTTP\/1.0 200 OK/{flag=1; next} flag; /./{flag=0}'
