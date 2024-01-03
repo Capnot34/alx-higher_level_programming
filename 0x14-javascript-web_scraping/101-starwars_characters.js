7cb5c4105214a996577e8b2e838e7a9fbabd1e2f
Capnot34 <jenniferamachree2@gmail.com>
2024-01-03 07:53:42 -0800

    last task done

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2]; // Get the Movie ID from the command line arguments
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error); // Print the error if it occurred
+  } else {
+    const movie = JSON.parse(body);
+
+    // Iterate through characters and print one per line in the right order
+    movie.characters.forEach((characterUrl, index) => {
+      request.get(characterUrl, (error, response, characterBody) => {
+        if (!error) {
+          const characterData = JSON.parse(characterBody);
+          console.log(characterData.name);
+
+          // Check if it's the last character, then print an empty line
+          if (index === movie.characters.length - 1) {
+            console.log('');
+          }
+        }
+      });
+    });
+  }
+});
