ef9b343664754ffb6f58df79c27ee6652a5b7177
Capnot34 <jenniferamachree2@gmail.com>
2024-01-03 06:11:18 -0800

    task 1 done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2];  // Get the file path from the command line arguments
+const filePath = process.argv[2]; // Get the file path from the command line arguments
 const content = process.argv[3]; // Get the string to write from the command line arguments
 
 // Write the content to the file asynchronously in utf-8 encoding
