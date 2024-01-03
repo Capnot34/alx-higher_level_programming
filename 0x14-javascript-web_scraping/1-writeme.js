3aa8c9813967679722bb4e6d0afe726dbb0d8828
Capnot34 <jenniferamachree2@gmail.com>
2024-01-03 06:08:57 -0800

    added some cods

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = resquire('fs');
+
+const filePath = process.argv[2];  // Get the file path from the command line arguments
+const content = process.argv[3]; // Get the string to write from the command line arguments
+
+// Write the content to the file asynchronously in utf-8 encoding
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err); // Print the error if it occurred
+  } else {
+    console.log(`Content written to ${filePath}`);
+  }
+});
